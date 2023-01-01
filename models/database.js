762f0c6baffda434beba22a201a5633caa882946
JosephHuy1906 <103304480+JosephHuy1906@users.noreply.github.com>
2023-01-01 14:37:01 +0700

    add

--- models/database.js
+++ models/database.js
@@ -1,9 +1,9 @@
 var mysql = require("mysql");
 var db = mysql.createConnection({
   host: "us-cdbr-east-06.cleardb.net",
-  user: "bc8ea6cff619c0",
-  password: "6a013c5e",
-  database: "heroku_5473ce749970e71",
+  user: "b085e9ba8811d4",
+  password: "50fd3802",
+  database: "heroku_a09b9395670425d",
 });
 db.connect(() => console.log("Da ket noi database !"));
 module.exports = db;
